607ac47fc0599fe6a14eaf1a5606a50e696cc972
Dmitriifd <idimka9@gmail.com>
2023-11-19 17:12:28 +0500

    CreateServerModal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 import { ThemeProvider } from '@/components/providers/theme-proveder';
+import { ModalProvider } from '@/components/providers/modal-provider';
 import { cn } from '@/lib/utils';
 import './globals.css';
 
@@ -30,6 +31,7 @@ export default function RootLayout({
             enableSystem={false}
             storageKey='discord-theme'
           >
+            <ModalProvider/>
             {children}
           </ThemeProvider>
         </body>
