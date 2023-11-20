d936ec7f3d864f4280b585ce50edfb31c817d669
Dmitriifd <idimka9@gmail.com>
2023-11-20 14:52:33 +0500

    MembersModal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -61,6 +61,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
         {isAdmin && (
           <DropdownMenuItem
+            onClick={() => onOpen('members', { server })}
             className='px-3 py-2 text-sm cursor-pointer'
           >
             Manage Members
@@ -68,26 +69,20 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isModerator && (
-          <DropdownMenuItem
-            className='px-3 py-2 text-sm cursor-pointer'
-          >
+          <DropdownMenuItem className='px-3 py-2 text-sm cursor-pointer'>
             Create Channel
             <PlusCircle className='h-4 w-4 ml-auto' />
           </DropdownMenuItem>
         )}
         {isModerator && <DropdownMenuSeparator />}
         {isAdmin && (
-          <DropdownMenuItem
-            className='text-rose-500 px-3 py-2 text-sm cursor-pointer'
-          >
+          <DropdownMenuItem className='text-rose-500 px-3 py-2 text-sm cursor-pointer'>
             Delete Server
             <Trash className='h-4 w-4 ml-auto' />
           </DropdownMenuItem>
         )}
         {!isAdmin && (
-          <DropdownMenuItem
-            className='text-rose-500 px-3 py-2 text-sm cursor-pointer'
-          >
+          <DropdownMenuItem className='text-rose-500 px-3 py-2 text-sm cursor-pointer'>
             Leave Server
             <LogOut className='h-4 w-4 ml-auto' />
           </DropdownMenuItem>
