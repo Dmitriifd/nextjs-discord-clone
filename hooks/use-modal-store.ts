bf8b7b74143a9f8f53ece119bf5c457b78565c1c
Dmitriifd <idimka9@gmail.com>
2023-11-20 15:36:19 +0500

    CreateChannelModal

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,10 +1,11 @@
 import { create } from 'zustand';
-import { Server } from "@prisma/client";
+import { Server, ChannelType } from "@prisma/client";
 
-export type ModalType = 'createServer' | 'invite' | 'editServer' | 'members';
+export type ModalType = 'createServer' | 'invite' | 'editServer' | 'members' | 'createChannel';
 
 interface ModalData {
   server?: Server
+  channelType?: ChannelType;
 }
 
 interface ModalStore {
